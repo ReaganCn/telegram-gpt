2c48d207b992bc0637e2aef85e0ce1e8c9c0ff23
ReaganCn <rgncharana2@gmail.com>
2023-04-23 19:25:43 +0300

    initial project setup

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/reagancn/telegram-gpt
+
+go 1.20
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/sashabaranov/go-openai v1.9.0
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	apiKey := os.Getenv("OPEN_AI_KEY")
+
+	client := openai.NewClient(apiKey)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello AI!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
