0404e5ea4d533c7dd4a55d4e366461dad31f3775
ReaganCn <rgncharana2@gmail.com>
2023-04-23 20:37:43 +0300

    feat: add telegram bot

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/sashabaranov/go-openai v1.9.0
 )
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
--- main.go
+++ main.go
@@ -6,27 +6,70 @@ import (
 	"log"
 	"os"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 	openai "github.com/sashabaranov/go-openai"
 )
 
+type BotClient struct {
+	id       string
+	openai   *openai.Client
+	telegram *tgbotapi.BotAPI
+}
+
 func main() {
+
 	err := godotenv.Load()
+
+	openAiKey := os.Getenv("OPEN_AI_KEY")
+	tBotToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	apiKey := os.Getenv("OPEN_AI_KEY")
+	botClient := &BotClient{}
 
-	client := openai.NewClient(apiKey)
-	resp, err := client.CreateChatCompletion(
+	botClient.openai = openai.NewClient(openAiKey)
+
+	botClient.telegram, err = tgbotapi.NewBotAPI(tBotToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	botClient.telegram.Debug = true
+
+	log.Printf("Authorized on account %s", botClient.telegram.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := botClient.telegram.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			responseText := botClient.sendToAI(update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, responseText)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			botClient.telegram.Send(msg)
+		}
+	}
+
+}
+
+func (b *BotClient) sendToAI(text string) string {
+	resp, err := b.openai.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
 			Model: openai.GPT3Dot5Turbo,
 			Messages: []openai.ChatCompletionMessage{
 				{
 					Role:    openai.ChatMessageRoleUser,
-					Content: "Hello AI!",
+					Content: text,
 				},
 			},
 		},
@@ -34,8 +77,7 @@ func main() {
 
 	if err != nil {
 		fmt.Printf("ChatCompletion error: %v\n", err)
-		return
 	}
 
-	fmt.Println(resp.Choices[0].Message.Content)
+	return resp.Choices[0].Message.Content
 }
