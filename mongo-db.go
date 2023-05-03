66425057ce1b73fffd859d3712d335300c5aa350
Reagan Charana <rgncharana2@gmail.com>
2023-05-03 22:15:23 +0300

    feat: integrate mongodb

---
+++ mongo-db.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func ConnectMongoDB(mongoURI string) (*mongo.Client, context.Context) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 600*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection by pinging the MongoDB server
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
+	// Return the client
+	return client, ctx
+}
+
+func GetCollection(client *mongo.Client, dbName string, collectionName string) *mongo.Collection {
+	return client.Database(dbName).Collection(collectionName)
+}
