4266af3c3a7c59daeb700137cf776c12cd931bb8
Reagan Charana <rgncharana2@gmail.com>
2023-05-03 22:15:48 +0300

    feat: initialize mongodb from main

--- main.go
+++ main.go
@@ -15,16 +15,23 @@ func main() {
 	// Get environment variables
 	openAiKey := os.Getenv("OPEN_AI_KEY")
 	tBotToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	mongoURI := os.Getenv("MONGO_URI")
 
 	// Check if .env file is loaded
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Connect to MongoDB and get client
+	mongoDBClient, ctx := ConnectMongoDB(mongoURI)
+
+	// Disconnect from MongoDB when main() ends
+	defer mongoDBClient.Disconnect(ctx)
+
 	// Initialize bot client
 	botClient := &BotClient{}
 
 	// Run bot
-	runBot(botClient, openAiKey, tBotToken)
+	runBot(botClient, mongoDBClient, ctx, openAiKey, tBotToken)
 
 }
