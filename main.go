e6d46c5cb872d8793780a3f8baf6bb3a55106eb4
Reagan Charana <rgncharana2@gmail.com>
2023-05-03 20:11:10 +0300

    doc: add comments

--- main.go
+++ main.go
@@ -9,17 +9,22 @@ import (
 
 func main() {
 
+	// Load .env file
 	err := godotenv.Load()
 
+	// Get environment variables
 	openAiKey := os.Getenv("OPEN_AI_KEY")
 	tBotToken := os.Getenv("TELEGRAM_BOT_TOKEN")
 
+	// Check if .env file is loaded
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Initialize bot client
 	botClient := &BotClient{}
 
+	// Run bot
 	runBot(botClient, openAiKey, tBotToken)
 
 }
